49a86b65dd3be10adff0b3dbd692e98d6a6d1772
ScottaBout <scott@boutfamily.com>
2021-12-02 15:55:55 -0600

    replaced print with logging, typecasted str to float for request.args.get

--- brain.py
+++ brain.py
@@ -4,14 +4,15 @@ import logging
 
 from drone_data import DroneData
 
-OPTITRACK = False # if false, assumes optitrack data equals drone data
+OPTITRACK = False  # if false, assumes optitrack data equals drone data
 
 app = Flask(__name__)
 
 drone_data_list = [DroneData(), DroneData()]  # global variable
 
-drone_data_list[0].ip = '192.168.1.157'  # TODO change ip address to drone address
-drone_data_list[1].ip = '10.194.94.228'  # TODO change ip address to drone address
+drone_data_list[0].ip = '192.168.1.158'  # TODO change ip address to drone address
+drone_data_list[1].ip = '192.168.1.157'  # TODO change ip address to drone address
+
 
 @app.route("/drone_data")
 def drone_data():
@@ -21,21 +22,21 @@ def drone_data():
         return Response('Missing drone_id', 500)
     logging.info(f'/drone_data for drone_id {drone_id}')
     if 'x' in request.args:
-        x = request.args.get('x')
+        x = float(request.args.get('x'))
         drone_data_list[drone_id].x = x
         if not OPTITRACK:
             drone_data_list[drone_id].opti_x = x
             if drone_data_list[drone_id].start_x is None:
                 drone_data_list[drone_id].start_x = x
     if 'y' in request.args:
-        y = request.args.get('y')
+        y = float(request.args.get('y'))
         drone_data_list[drone_id].y = y
         if not OPTITRACK:
             drone_data_list[drone_id].opti_y = y
             if drone_data_list[drone_id].start_y is None:
                 drone_data_list[drone_id].start_y = y
     if 'z' in request.args:
-        z = request.args.get('z')
+        z = float(request.args.get('z'))
         drone_data_list[drone_id].z = z
         if not OPTITRACK:
             drone_data_list[drone_id].opti_z = z
@@ -44,31 +45,32 @@ def drone_data():
     response = f'Drone: {drone_id} : ip = {drone_data_list[drone_id].ip}, x = {drone_data_list[drone_id].x}, y = {drone_data_list[drone_id].y}, z = {drone_data_list[drone_id].z} '
     logging.info(response)
     recalculate()
-    print('recalculating')
+    logging.info('recalculating')
     return Response(response)
 
 
 def recalculate():
-    print('entered recalculation')
+    logging.info('entered recalculation')
     drone0 = drone_data_list[0]
     drone1 = drone_data_list[1]
     if drone0.start_x is None or drone1.start_x is None:
         return
     if drone0.real_z() < 0.5:
-        print('setting target for drone 0')
+        logging.info('setting target for drone 0')
         drone0.set_target(drone0.start_x, drone0.start_y, 0.55)
     if drone1.real_z() < 0.5:
-        print('setting target for drone 1')
+        logging.info('setting target for drone 1')
         drone1.set_target(drone1.start_x, drone1.start_y, 0.55)
     if drone0.real_z() > 0.5 and drone1.real_z() > 0.5:
         drone0.set_target(1, 2, 1)
-        h = drone0.heading(drone1)
-        x, y = drone0.relative(2, h)
-        drone1.set_target(x, y, 1)
+        drone1.set_target(1, 2, 1)
+        # h = drone0.heading(drone1)
+        # x, y = drone0.relative(2, h)
+        # drone1.set_target(x, y, 1)
     send_to_drone(drone0)
-    print('sending to drone 0')
+    logging.info('sending to drone 0')
     send_to_drone(drone1)
-    print('sending to drone 1')
+    logging.info('sending to drone 1')
 
 
 def send_to_drone(drone_data: DroneData):
@@ -77,16 +79,17 @@ def send_to_drone(drone_data: DroneData):
     :param drone_data:
     :return:
     """
-    print('send to drone activated')
+    logging.info('send to drone activated')
     if drone_data.ip != '':
-        print(f'ip found at {drone_data.ip}, trying')
+        logging.info(f'ip found at {drone_data.ip}, trying')
         try:
-            response = requests.get(f'http://{drone_data.ip}:8080/drone_target', params=drone_data.string_dict())
+            response = requests.get(f'http://{drone_data.ip}:8000/drone_target', params=drone_data.string_dict())
             if response.status_code != 200:
-                print(f'Error code sending request {response.status_code}')
+                logging.info(f'Error code sending request {response.status_code}')
             else:
-                print(f'url: {response.url}')
-                print(f'response: {response.content}')
+                print('HTTP Connection established')
+                logging.info(f'url: {response.url}')
+                logging.info(f'response: {response.content}')
         except requests.exceptions.RequestException as err:
             logging.warning(f'error: {err}')
     else:
